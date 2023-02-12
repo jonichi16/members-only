933d03308fe54ad1ed0b59319ddaeb64d669fb12
Jhonel Villanueva <vvjhonel@gmail.com>
2023-02-12 13:41:34 +0800

    Add username authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    shared: 'users/shared'
+  }
   root 'home#index'
   resources :posts, only: [:new, :create, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
